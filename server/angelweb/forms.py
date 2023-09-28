c57d4ab6793ab90f8c6ff857af6bc14efb4c743f
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-28 17:40:36 +0200

    adding styling

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class BamForm(forms.Form):
-    url_link = forms.CharField(max_length=100, required=False, help_text="<span class='hovertext' data-hover='Search for a specific variation that query will look for'>Mutated Allele</span>", label="")
+    url_link = forms.CharField(max_length=100, required=False, help_text="<span class='hovertext' data-hover='Introduce the URI for the beacon to be verified'>Beacon URI</span>", label="")
 
     
